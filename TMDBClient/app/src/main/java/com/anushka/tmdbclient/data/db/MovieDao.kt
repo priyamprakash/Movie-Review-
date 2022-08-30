1e996b17c4e9e80f72f7297ad0a0ae57871f02f7
Priyam Prakash <priyamprakash99@gmail.com>
2022-08-31 04:35:59 +0530

    Update MovieDao.kt

--- TMDBClient/app/src/main/java/com/anushka/tmdbclient/data/db/MovieDao.kt
+++ TMDBClient/app/src/main/java/com/anushka/tmdbclient/data/db/MovieDao.kt
@@ -5,7 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.anushka.tmdbclient.data.model.movie.Movie
-
+//Da0 - Data Access Objects
 @Dao
 interface MovieDao {
 
